ca7cbe87e75d6f3205ed2050de742e341533d48e
thiggena <t.iggena@hs-osnabrueck.de>
2021-02-03 23:02:52 +0100

    finished with section 22, task 3

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -24,29 +24,35 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        Publisher antonPublisher = new Publisher("Anton", "Street 1", "Onetown", "Bla", "134245");
+        publisherRepository.save(antonPublisher);
+        System.out.println("Number of publishers: " + publisherRepository.count());
+
         Author otto = new Author("Otto", "Ottosen");
         Book aBook = new Book("aBook", "12345");
         otto.getBooks().add(aBook);
         aBook.getAuthors().add(otto);
+        aBook.setPublisher(antonPublisher);
+        antonPublisher.getBooks().add(aBook);
 
         authorRepository.save(otto);
         bookRepository.save(aBook);
+        publisherRepository.save(antonPublisher);
 
         Author heinz = new Author("Heinz", "Heinzsen");
         Book bBook = new Book("bBook", "54321");
         heinz.getBooks().add(bBook);
         bBook.getAuthors().add(heinz);
+        bBook.setPublisher(antonPublisher);
+        antonPublisher.getBooks().add(bBook);
 
         authorRepository.save(heinz);
         bookRepository.save(bBook);
+        publisherRepository.save(antonPublisher);
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of books: " + bookRepository.count());
 
-        Publisher antonPublisher = new Publisher("Anton", "Street 1", "Onetown", "Bla", "134245");
-
-        publisherRepository.save(antonPublisher);
-        System.out.println("Number of publishers: " + publisherRepository.count());
-
+        System.out.println("Publisher number of books: " + antonPublisher.getBooks().size());
     }
 }
